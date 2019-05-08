4acfd826fa9e3f60b419eea39a3b801d231af290
Mohammed Kanchwala <mkanchwala@farabi.ae>
2019-05-08 12:55:17 +0400

    no message

--- src/test/java/com/mk/retailstore/RetailStoreApplicationTests.java
+++ src/test/java/com/mk/retailstore/RetailStoreApplicationTests.java
@@ -2,7 +2,7 @@ package com.mk.retailstore;
 
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
