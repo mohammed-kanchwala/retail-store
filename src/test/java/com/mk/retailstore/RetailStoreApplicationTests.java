9e45141e6d86ba3f5216ba5e37ff99b70f33da6f
Mohammed Kanchwala <mkanchwala@farabi.ae>
2019-05-08 12:56:55 +0400

    no message

--- src/test/java/com/mk/retailstore/RetailStoreApplicationTests.java
+++ src/test/java/com/mk/retailstore/RetailStoreApplicationTests.java
@@ -2,7 +2,7 @@ package com.mk.retailstore;
 
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -11,7 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(classes = RetailStoreApplication.class)
 public class RetailStoreApplicationTests {
 
-	@Before
+	@Test
 	public void contextLoads() {
 		assertTrue(true);
 	}
